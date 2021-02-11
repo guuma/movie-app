d0233ab05283d5403af1aee6b0d88483c853e95d
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2021-02-11 14:33:13 +0900

    Create VideoDetail page

--- src/components/VideoDetail/VideoDetail.jsx
+++ src/components/VideoDetail/VideoDetail.jsx
@@ -1,33 +1,43 @@
-import React, { useContext, useEffect } from 'react'
-import { useLocation }from 'react-router-dom'
-import { fetchSelectedData } from '../../apis'
-import { Store } from '../../store/index'
+import React, { useContext, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { fetchSelectedData } from '../../apis';
+import { Store } from '../../store/index';
+import VideoPlay from '../VideoPlay/VideoPlay';
+import Style from './VideoDetail.module.scss';
+import Linkfy from 'react-linkify';
 
 const VideoDetail = () => {
-  const { globalState, setGlobalState } = useContext(Store)
-  const location = useLocation()
+  const { globalState, setGlobalState } = useContext(Store);
+  const location = useLocation();
   const setSelectedVideo = async () => {
     const searchParams = new URLSearchParams(location.search);
     const id = searchParams.get('v');
     await fetchSelectedData(id).then((res) => {
-      const item =  res.data.items.shift()
-      console.log('res', res)
+      const item = res.data.items.shift();
+      console.log('res', res);
       setGlobalState({
         type: 'SET_SELECTED',
-        payload:  {
+        payload: {
           selected: item,
-        }
-      })
-    })
-  }
+        },
+      });
+    });
+  };
   useEffect(() => {
-    setSelectedVideo()
-  }, [])
-  return (
-    <div>
-
+    setSelectedVideo();
+  }, []);
+  return globalState.selected && globalState.selected.id ? (
+    <div className={Style.wrap}>
+      <VideoPlay id={globalState.selected.id} />
+      <p>{globalState.selected.snippet.title}</p>
+      <hr />
+      <Linkfy>
+        <pre>{globalState.selected.snippet.description}</pre>
+      </Linkfy>
     </div>
-  )
-}
+  ) : (
+    <span>No Data！！</span>
+  );
+};
 
-export default VideoDetail
+export default VideoDetail;
